8e32f3c26fc7e830fb540853fc236734bce71605
Enes Çakar <progames5252@gmail.com>
2022-02-05 00:45:51 +0300

    container fields

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -47,14 +47,91 @@ class HomeBody extends StatelessWidget {
                         ),
                         fit: BoxFit.cover),
                   ),
-                  child: Column(
-                    children: [
-                      Text("aaa"),
-                      Text("aaa"),
-                      Text("aaa"),
-                      Text("aaa"),
-                      Text("aaa"),
-                    ],
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            width: 2, color: Colors.white))),
+                                child: const Padding(
+                                  padding: EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    "İstanbul",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Column(
+                                      children: const [
+                                        Text(
+                                          "Hicri",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 18),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            "4 Recep 1443",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 16),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: const [
+                                        Text(
+                                          "Miladi",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 18),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            "05 Şubat 2022 Cumartesi",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 16),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   )),
             ),
           )
