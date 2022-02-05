cafe1571c9e4e72044cdcd7e964174a592add1b3
Enes Çakar <progames5252@gmail.com>
2022-02-05 13:32:20 +0300

    add field

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -12,8 +12,14 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
-        leading: Icon(Icons.menu),
+        backgroundColor: Colors.blueGrey.shade700,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          tooltip: "Menu Button",
+          onPressed: () {
+            print("Menu Button On Clicked");
+          },
+        ),
         title: const Text("Anasayfa"),
       ),
       body: const HomeBody(),
@@ -60,73 +66,9 @@ class HomeBody extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Container(
-                                decoration: const BoxDecoration(
-                                    border: Border(
-                                        bottom: BorderSide(
-                                            width: 2, color: Colors.white))),
-                                child: const Padding(
-                                  padding: EdgeInsets.only(bottom: 5),
-                                  child: Text(
-                                    "İstanbul",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w900,
-                                        fontSize: 20),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 20),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Column(
-                                      children: const [
-                                        Text(
-                                          "Hicri",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: 18),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.only(top: 5),
-                                          child: Text(
-                                            "4 Recep 1443",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 16),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Column(
-                                      children: const [
-                                        Text(
-                                          "Miladi",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: 18),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.only(top: 5),
-                                          child: Text(
-                                            "05 Şubat 2022 Cumartesi",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 16),
-                                          ),
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              )
+                              const City(),
+                              const Dates(),
+                              Vakits(),
                             ],
                           )
                         ],
@@ -134,8 +76,194 @@ class HomeBody extends StatelessWidget {
                     ),
                   )),
             ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 25),
+            child: KalanSure(),
           )
         ],
+      ),
+    );
+  }
+
+  Column Vakits() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              VakitTexts("İmsak"),
+              VakitTexts("Güneş"),
+              VakitTexts("Öğle"),
+              VakitTexts("İkindi"),
+              VakitTexts("Akşam"),
+              VakitTexts("Yatsı"),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              VakitSaatTexts("05.30"),
+              VakitSaatTexts("06.30"),
+              VakitSaatTexts("13.10"),
+              VakitSaatTexts("16.30"),
+              VakitSaatTexts("18.30"),
+              VakitSaatTexts("19.30"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Text VakitSaatTexts(String saat) {
+    return Text(
+      saat,
+      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+    );
+  }
+
+  Text VakitTexts(String vakit) {
+    return Text(
+      vakit,
+      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+    );
+  }
+}
+
+class KalanSure extends StatelessWidget {
+  const KalanSure({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.network(
+          "https://namazvakti.diyanet.gov.tr/images/r4.gif",
+          width: 100,
+        ),
+        Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 5),
+              child: Text(
+                "Öğle",
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 5),
+              child: Text(
+                "Vaktin Çıkmasına Kalan Süre",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+              ),
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    "02.30",
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                  ),
+                ),
+                Text(
+                  "Saat",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class Dates extends StatelessWidget {
+  const Dates({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: const [
+              Text(
+                "Hicri",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  "4 Recep 1443",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: const [
+              Text(
+                "Miladi",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  "05 Şubat 2022 Cumartesi",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class City extends StatelessWidget {
+  const City({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(width: 2, color: Colors.white))),
+      child: const Padding(
+        padding: EdgeInsets.only(bottom: 5),
+        child: Text(
+          "İstanbul",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w900, fontSize: 18),
+        ),
       ),
     );
   }
